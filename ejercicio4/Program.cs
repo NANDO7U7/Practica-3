5964bbf83214a9d974f69cf0c5e3aff87a35af67
FERNANDO JOSE CRUZ CHEVEZ <SMSS141122@ugb.edu.sv>
2022-08-15 22:21:46 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace App5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            do
+            {
+                Console.WriteLine("Ingrese un numero entero (con 0 se finaliza): ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num >= 100)
+                {
+                    Console.WriteLine("Tiene 3 digitos.");
+                }
+                else if (num >= 10)
+                {
+                    Console.WriteLine("Tiene 2 digitos.");
+                }
+                else
+                {
+                    Console.WriteLine("Tiene 1 digitos.");
+
+                }
+            } while (num != 0);
+        }
+
+    }
+}
---
+++ ejercicio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practica_presencial_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nota promedio: ");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 0 && nota <= 10)
+
+                if (nota >= 6)
+
+                    Console.WriteLine("Aprovado");
+
+                else if (nota >= 5)
+                    Console.WriteLine("Reposición");
+
+                else
+
+                    Console.WriteLine("Reprobado");
+
+            else
+
+                Console.WriteLine("La nota no es correcta");
+
+        }
+    }
+}
